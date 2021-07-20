cc3bd80b6b4d7c5c25d84a6dd6fae30a52093df6
ZenithGD <darimarcos456@hotmail.com>
2021-07-20 20:04:49 +0200

    Add simple battle commands

--- src/cogs/pb_admin.py
+++ src/cogs/pb_admin.py
@@ -1,3 +1,12 @@
 import discord
 from discord.ext import commands
 
+
+class PokeBotAdmin(commands.Cog):
+
+    # Ensure the user has the BotAdmin role
+    @commands.has_role("BotAdmin")
+    @commands.command(name="show battles")
+    async def show_battles(self, ctx):
+
+
